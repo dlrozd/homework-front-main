929c7b205888990810efbeab16ae2a4352b8b30d
drozd <mayakovsky03@gmail.com>
2025-12-31 19:03:26 +0200

    practice props, types, components

---
+++ src/HW/2/UserList2.tsx
@@ -0,0 +1,23 @@
+import {UsersObjectType} from './HW2';
+import {CurrentUser} from "./CurrentUser";
+
+type UserList2PropsType = {
+	users: UsersObjectType // пропиши типизацию
+	filterUsers: any// пропиши типизацию
+};
+
+export const UserList2 = (props: UserList2PropsType) => {
+	return (
+		<div id={'hw02-users'}>
+			<h2>User List:</h2>
+
+			<button id={'hw02-filter-button'} onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
+
+			<ul>
+			  {props.users.myFriends.map((user) => (
+			    <CurrentUser key={user.id} user={user}/>
+			  ))}
+			</ul>
+		</div>
+	);
+};
